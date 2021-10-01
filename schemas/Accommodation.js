ea4ec789ba8c88cc2ca79a34fba27d0a16e8e8d7
Tomas Berhane <68202596+tomas-collab@users.noreply.github.com>
2021-10-01 11:20:25 +0200

    accomodation schema

--- schemas/Accommodation.js
+++ schemas/Accommodation.js
@@ -5,8 +5,8 @@ const {Schema,model} = mongoose
 const Accomodation = new Schema({
     
     name:{type:String,required:true},
-    host:{type:Schema.Types.ObjectId,ref:"User"},
-    description:{type:Text},
+    host:{type:Schema.Types.ObjectId,required:true,ref:"user"},
+    description:{type:Text,required:true},
     maxGuests:{type:Number,required:true},
     city:{type:String,required:true}
 },
