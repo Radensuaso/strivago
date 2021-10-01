655bedcbeeeced3a1ab5dd7fbf5617614ce73758
André Sousa <66281967+Radensuaso@users.noreply.github.com>
2021-10-01 15:19:12 +0100

    Accommodations with auth done

--- src/services/accommodations/index.js
+++ src/services/accommodations/index.js
@@ -1,12 +1,15 @@
 import express from "express";
 import AccommodationModel from "../../../schemas/Accommodation.js";
+import { tokenMiddleware } from "../../auth/tokenMiddleware.js";
+import { hostMiddleware } from "../../auth/hostMiddleware.js";
 import createHttpError from "http-errors";
+import usersRouter from "../users/index.js";
 
 const accommodationsRouter = express.Router();
 
 accommodationsRouter
   .route("/")
-  .get(async (req, res, next) => {
+  .get(tokenMiddleware, async (req, res, next) => {
     try {
       const allAccommodation = await AccommodationModel.find().populate("host");
       res.send(allAccommodation);
@@ -14,7 +17,7 @@ accommodationsRouter
       next(error);
     }
   })
-  .post(async (req, res, next) => {
+  .post(tokenMiddleware, hostMiddleware, async (req, res, next) => {
     try {
       const newAccommodation = new AccommodationModel(req.body);
       const accommodation = await newAccommodation.save();
@@ -26,36 +29,61 @@ accommodationsRouter
 
 accommodationsRouter
   .route("/:accId")
-  .get(async (req, res, next) => {
+  .get(tokenMiddleware, async (req, res, next) => {
     try {
+      const { accId } = req.params;
       const oneAccommodation = await AccommodationModel.findById(
-        req.params.accId
-      );
-      res.send(oneAccommodation);
+        accId
+      ).populate("host");
+
+      if (oneAccommodation) {
+        res.send(oneAccommodation);
+      } else {
+        next(createHttpError(404, "This accommodation is not found"));
+      }
     } catch (error) {
       next(error);
     }
   })
-  .put(async (req, res, next) => {
+  .put(tokenMiddleware, hostMiddleware, async (req, res, next) => {
     try {
-      const AccId = req.params.accId;
-      const updateAccommodation = await AccommodationModel.findByIdAndUpdate(
-        AccId,
+      const { accId } = req.params;
+      const updateAccommodation = await AccommodationModel.findOneAndUpdate(
+        { _id: accId, host: req.user._id },
         req.body,
         { new: true }
       );
-      res.send(updateAccommodation);
+      if (updateAccommodation) {
+        res.send(updateAccommodation);
+      } else {
+        next(
+          createHttpError(
+            401,
+            "You are not authorized to change this accommodation."
+          )
+        );
+      }
     } catch (error) {
       next(error);
     }
   })
-  .delete(async (req, res, next) => {
+  .delete(tokenMiddleware, hostMiddleware, async (req, res, next) => {
     try {
-      const AccId = req.params.accId;
-      const deletedAccommodation = await AccommodationModel.findByIdAndDelete(
-        AccId
-      );
-      res.send(deletedAccommodation);
+      const { accId } = req.params;
+      const deletedAccommodation = await AccommodationModel.findOneAndDelete({
+        _id: accId,
+        host: req.user._id,
+      });
+      if (deletedAccommodation) {
+        res.send(deletedAccommodation);
+      } else {
+        next(
+          createHttpError(
+            401,
+            "You are not authorized to change this accommodation."
+          )
+        );
+      }
     } catch (error) {
       next(error);
     }
